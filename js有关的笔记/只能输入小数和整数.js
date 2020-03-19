e71fe435d1918ab519aa287e04883678d85b6374
zhushaopeng <zhushaopeng@ybm100.com>
2020-03-19 11:39:07 +0800

    '添加小数校验js代码'

--- js有关的笔记/只能输入小数和整数.js
+++ js有关的笔记/只能输入小数和整数.js
@@ -33,3 +33,13 @@ function jiaoyan(value) {
                 return  r;
             }
         }
+
+//只能输入整数或者小数(保留两位小数)
+  function num(obj){
+      obj.value = obj.value.replace(/[^\d.]/g,""); //清除"数字"和"."以外的字符
+      obj.value = obj.value.replace(/^\./g,""); //验证第一个字符是数字
+      obj.value = obj.value.replace(/\.{2,}/g,"."); //只保留第一个, 清除多余的
+      obj.value = obj.value.replace(".","$#$").replace(/\./g,"").replace("$#$",".");
+      obj.value = obj.value.replace(/^(\-)*(\d+)\.(\d\d).*$/,'$1$2.$3'); //只能输入两个小数
+  }
+
